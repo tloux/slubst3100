3ebfb02cd3533f3acf58247b5fed0a1ad5d1bd57
Travis <travis.loux@gmail.com>
2019-06-17 14:46:29 -0500

    Add gss, nhp data sets

---
+++ R/gss.R
@@ -0,0 +1,13 @@
+#' Marijuana legalization
+#'
+#' Geneal Social Survey results from a question asking "Do you think the use of
+#' marijuana should be made legal, or not?"
+#'
+#' @format A data frame with 1578 rows and 2 variables: 
+#'  \describe{
+#'   \item{id}{Participant identification number}
+#'   \item{legalize}{Respondent's response to question}
+#'  }
+#' @source Data set created from the summarized data in 4th edition of
+#'   \url{https://www.openintro.org/stat/textbook.php} (pp 216).
+"gss"
---
+++ R/nhp.R
@@ -0,0 +1,13 @@
+#' National health plan
+#'
+#' Responses to a Kaiser Family Foundation poll asking US adults whether they
+#' supported a national health plan.
+#'
+#' @format A data frame with 1262 rows and 2 variables: 
+#'  \describe{
+#'   \item{political}{Political party repondent identified with} 
+#'   \item{support}{Whether respondent supported a national health plan}
+#'  }
+#' @source Data set created from the summarized data in 4th edition of
+#'   \url{https://www.openintro.org/stat/textbook.php} (pp 216).
+"nhp"
