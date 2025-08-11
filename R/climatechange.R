4afa9336d9a67a84a9b3383688b956f3cabd7383
loux <loux@slu.edu>
2025-08-11 09:53:15 -0500

    0.6.0 updates for fall 2025
    Added climatechange data set
    Also updated type in quitters documentation

---
+++ R/climatechange.R
@@ -0,0 +1,15 @@
+#' Act on climate change
+#'
+#' Data from a study of Pew Research poll asking respondents whether 
+#' they have personally taken action to help address climate change 
+#' within the last year.
+#'
+#' @format A data frame with 13664 rows and 2 variables:
+#'  \describe{
+#'   \item{generation}{Respondent's generation cohort}
+#'   \item{action}{Whether respondent reported taking action to 
+#'   address climate change in last year}
+#'  }
+#' @source Data set created from the summarized data in 2nd edition of
+#'   \url{https://www.openintro.org/book/ims/} (pp 311).
+"climatechange"
